cc8810e763fa95656e9d0b56a7c08305f97a7ddc
Ng <Nguyenbdnghi@gmail.com>
2024-01-01 09:47:25 +0100

    Add LESS module

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+    preprocessorOptions: {
+      less: {
+        relativeUrls: true,
+        javascriptEnabled: true,
+      },
+    },
+  },
 })
